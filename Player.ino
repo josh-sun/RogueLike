eb652b95e933d0254c412583bd30fbbeb08bc038
andyyleeh <hs27lee@edu.uwaterloo.ca>
2016-11-24 14:36:22 -0500

    Projectile and Monster file updated

--- Player.ino
+++ Player.ino
@@ -1,1 +1,15 @@
+struct position{
+  int x,y;
+}
+
+struct player{
+  int health, x, y;
+}
+
+struct player CreatePlayer (int x, int y, int health){
+  struct player Player1 = {x,y,health};
+  return Player1;
+}
+
+
 
